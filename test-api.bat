41be4abfb5ded6b4ab1055b8f9537fcb5c0a3c32
filagate03 <alexfarley555@gmail.com>
2025-09-08 01:29:14 +0300

    add test-api.bat

---
+++ test-api.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Testing Шашлык-Машлык API...
+echo.
+echo Make sure the backend server is running before testing!
+echo.
+echo Press any key to continue...
+pause >nul
+echo.
+node test-api.js
+echo.
+echo Press any key to exit...
+pause >nul
