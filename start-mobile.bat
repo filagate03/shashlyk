980c700bcd6189689f79c33f7fd77429172c8b0a
filagate03 <alexfarley555@gmail.com>
2025-09-08 01:29:12 +0300

    add start-mobile.bat

---
+++ start-mobile.bat
@@ -0,0 +1,6 @@
+@echo off
+echo To run the mobile app, you need to use Expo CLI.
+echo Please install Expo CLI globally by running: npm install -g expo-cli
+echo Then navigate to the mobile directory and run: npm start
+echo Alternatively, you can open the mobile app in a web browser by navigating to:
+echo http://localhost:3000
